1594029081aa90ce536daf5ff6bb205523cbb371
Mehreen Hurroo <74601261+MehreenHurroo@users.noreply.github.com>
2021-02-01 03:15:13 +0400

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAjiOqqRsWTyMrINfLgK5AdA-xDv2SWM_g",
+    authDomain: "whatsapp-d5325.firebaseapp.com",
+    projectId: "whatsapp-d5325",
+    storageBucket: "whatsapp-d5325.appspot.com",
+    messagingSenderId: "331984619529",
+    appId: "1:331984619529:web:0143a5c0714143eda5d0d8"
+  };
